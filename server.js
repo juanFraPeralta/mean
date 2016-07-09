6c6f0c88da402b6629801cee4cec1578f6e1d7e0
Admin <admin@A51-06.local>
2016-07-08 21:50:15 -0500

    Second Demo Version

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ var path = require('path');
 app.get('/', function(request, response){
 	response.sendFile(path.join(__dirname) + '/index.html');
 });
-app.listen(1337);
+//app.listen(1337);
 console.log('Here we go!');
 
