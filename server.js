66e970afee5faced3ff38944b1aaca8ec82dcacf
Juan Peralta <juanfraperalta@gmail.com>
2016-07-15 19:44:20 -0500

    Clase 5 - 1

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/pokemon');
+//mongoose.connect('mongodb://admin:123@ds017553.mlab.com:17553/pokemon_juanfraperalta');
+//console.log(mongoose);
+
+//Call the packages
 var express = require('express');
 var app = express();
 var path = require('path');
 var adminRouter = express.Router();
 var loginRouter = express.Router();
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var port = process.env.PORT || 5000;
+//
+
+//APP CONFIGURATION
+app.use(bodyParser.urlencoder({ extended:true }));
+app.use(bodyParser.json());
+
+//CORS
+app.use(function(req, res, next){
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization');
+});
+
+app.use(morgan())
 
 app.get('/', function(request, response){
 	response.sendFile(path.join(__dirname) + '/index.html');
