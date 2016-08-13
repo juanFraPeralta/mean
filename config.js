111269478a0729894c848b27344e63d9389f61b2
Juan Peralta <juanfraperalta@gmail.com>
2016-08-12 20:29:50 -0500

    Update connection string to mlab

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   'port': process.env.PORT || 5000,
-  'database': 'mongodb://localhost/pokemon',
-  //'database': 'mongodb://admin:peptic.far@ds021434.mlab.com:21434/pokemon_yfx',
+  //'database': 'mongodb://localhost/pokemon',
+  'database': 'mongodb://admin:123@ds017553.mlab.com:17553/pokemon_juanfraperalta',
   'superSecret': 'strangeThingsHappendInArea51'
 }
